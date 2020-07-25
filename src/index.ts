c16fc3ad57a023bdfe44728c050d02db79b51b20
garug <allefff@gmail.com>
2020-07-25 09:52:54 -0300

    [add] Handling with NegocioError globally

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import connectDatabase from './database';
 import routes from './routes';
+import NegocioError from './NegocioError';
 
 const port = process.env.PORT || 3000;
 
@@ -12,5 +13,15 @@ const app = express();
 connectDatabase();
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof NegocioError) {
+        const { status, msgs } = err;
+        return res.status(status).send({ msgs, timestamp: new Date() });
+    }
+    console.error(err);
+    res.status(500);
+    return res.render('error', { error: err });
+});
 
 app.listen(port, () => console.log(`Server started! Port: ${port}`));
